ff6aa35890494477ffb6f5d6743152dd93d9f54c
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-02 21:07:22 -0500

    Added scripts in package.json to enabale express and react to run simultaneously and in heroku enable just the server.

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ const PORT= process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+// Added mongoose config object to the.connect method
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/track-your-subscription",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  );
+  
+  const connection = mongoose.connection;
+  
+  connection.on("connected", () => {
+    console.log("Mongoose successfully connected!");
+  });
+  
+  connection.on("error", (err) => {
+    console.log("Mongoose connection error: ", err);
+  });
 // Add  a get route to test
 
 app.get("/api/config", (req,res) => {
