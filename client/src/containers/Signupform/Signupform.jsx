1dd4c149c56a722ecaf86a999bad7a437a29aaa1
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-03 16:49:35 -0500

    Created a components folder and added a navbar. Created  containers and added signupform and subscription.jsx in src folder. Added materialize cd in index.html.

---
+++ client/src/containers/Signupform/Signupform.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signupform = () => {
+    return (
+        <div>
+            <h1>This is the sign up form </h1>
+        </div>
+    );
+};
+
+export default Signupform;
