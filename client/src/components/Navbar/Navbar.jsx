381d094dbf63f0c73e0beb72e585acebfd68a4b9
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-03 17:11:26 -0500

    Installed the react-router-dom in the client folder.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import navbar from './components/Navbar'
 
 const navbar = () => {
     return (
         <div>
-            <h1>this is the navbar</h1>
+           {/* <!-- Dropdown Structure --> */}
+<ul id="dropdown1" class="dropdown-content">
+  <li><a href="#!">one</a></li>
+  <li><a href="#!">two</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">signout</a></li>
+</ul>
+<nav>
+  <div class="nav-wrapper">
+    <a href="#!" class="brand-logo">Logo</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="sass.html">Sass</a></li>
+      <li><a href="badges.html">Components</a></li>
+ 
+      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+    </ul>
+  </div>
+</nav>
         </div>
     );
 };
---
+++ client/src/containers/Subscriptions/Subscriptions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Subscriptions = () => {
+    return (
+        <div>
+            <h1>this are subscriptions</h1>
+        </div>
+    );
+};
+
+export default Subscriptions;
